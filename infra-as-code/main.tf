bb9eba65445683f6103cfb13fb16bec8f0834cbd
Rafa Adorna <rafael.adorna@gmail.com>
2022-08-22 09:06:24 +0200

    Init main.tf

---
+++ infra-as-code/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "~> 1.1.7"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "< 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = var.region
+}
+
+# The backend config variables come from a backend.tfvars file
+# which is not in this repo
+terraform {
+  backend "s3" {
+  }
+}
