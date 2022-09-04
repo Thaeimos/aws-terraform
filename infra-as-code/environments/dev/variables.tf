c3a42d7d5c38783ea19597a7c485555629424fe5
Rafa Adorna <rafael.adorna@gmail.com>
2022-09-04 16:47:59 +0200

    Init variable file

--- infra-as-code/environments/dev/variables.tf
+++ infra-as-code/environments/dev/variables.tf
@@ -1,26 +1,4 @@
-
-variable "s3_bucket" {
-  description = "S3 bucket for terraform state."
-  type        = string
-}
-
-variable "key" {
-  description = "Prefix and routes for the terraform state"
-  type        = string
-}
-
 variable "region" {
-  description = "'Name' tag for S3 bucket with terraform state."
+  description = "The region to deply the resources"
   type        = string
-}
-
-variable "dynamodb_table" {
-  description = "DynamoDB table name for terraform lock."
-  type        = string
-}
-
-variable "bucket_state_encryption" {
-  type        = bool
-  description = "Encryption enabled for the bucket that holds the state"
-  default     = true
 }
