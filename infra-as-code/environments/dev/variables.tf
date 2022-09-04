39d31cd11d459beac59c6feabfdab96c087f7215
Rafa Adorna <rafael.adorna@gmail.com>
2022-09-04 17:10:48 +0200

    Add server port

--- infra-as-code/environments/dev/variables.tf
+++ infra-as-code/environments/dev/variables.tf
@@ -2,3 +2,9 @@ variable "region" {
   description = "The region to deply the resources"
   type        = string
 }
+
+variable "server_port" {
+  description = "The port where the HTTP server listens for requests"
+  type        = string
+  default     = "8080"
+}
