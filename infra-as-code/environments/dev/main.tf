f2e9925c48d4be9629095463adf2122fa3fbcfd5
Rafa Adorna <rafael.adorna@gmail.com>
2022-09-04 17:29:46 +0200

    Working example for testing structure

--- infra-as-code/environments/dev/main.tf
+++ infra-as-code/environments/dev/main.tf
@@ -12,10 +12,69 @@ provider "aws" {
   region  = var.region
 }
 
-# The backend config variables come from a backend.tfvars file
-# which is not in this repo
-# Terraform init -backend-config=
+# The backend config variables come from a backend.conf.secrets file
 terraform {
   backend "s3" {
+  }
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = var.zone
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_network_interface" "foo" {
+  subnet_id   = aws_subnet.my_subnet.id
+  private_ips = ["172.16.10.100"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+resource "aws_instance" "example" {
+  # Ubuntu Server 18.04 LTS (HVM), SSD Volume Type in us-east-2
+  ami                    = "ami-0bb7db69de29bbf1c"
+  instance_type          = "t2.micro"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
+
+  network_interface {
+    network_interface_id = aws_network_interface.foo.id
+    device_index         = 0
+  }
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name    = "terraform-example-instance"
+  vpc_id  = aws_vpc.my_vpc.id
+
+  # Inbound HTTP from anywhere
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
