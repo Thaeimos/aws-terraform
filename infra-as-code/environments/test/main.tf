f6173e5197620fcc4b07764bb54b89a7a7192ff2
Rafa Adorna <rafael.adorna@gmail.com>
2022-09-23 07:25:39 +0200

    Start user creation part

--- infra-as-code/environments/test/main.tf
+++ infra-as-code/environments/test/main.tf
@@ -22,6 +22,29 @@ provider "aws" {
   region  = var.region
 }
 
+# Users creation
+resource "aws_iam_user" "user" {
+  for_each      = toset(var.read_only_users)
+  name          = each.value
+  path          = "/"
+  force_destroy = true
+}
+
+provisioner "local-exec" {
+  command = "echo ${self.private_ip} >> private_ips.txt"
+ }
+
+resource "aws_iam_user_login_profile" "user" {
+  for_each      = toset(var.read_only_users)
+  user    = "${aws_iam_user.user[each.key].name}"
+  pgp_key = "keybase:your_keybase_username"
+}
+
+# output "password" {
+#   value = "${aws_iam_user_login_profile.user[each.key].encrypted_password}"
+# }
+
+# VPC
 resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support   = true
@@ -129,79 +152,79 @@ resource "aws_iam_instance_profile" "ecs_agent" {
   role = aws_iam_role.ecs_agent.name
 }
 
-# Autoscaling group
-### DECIDE WHAT TO DO WITH AMI
-resource "aws_launch_configuration" "ecs_launch_config" {
-    image_id             = "ami-094d4d00fd7462815"
-    iam_instance_profile = aws_iam_instance_profile.ecs_agent.name
-    security_groups      = [aws_security_group.ecs_sg.id]
-    user_data            = "#!/bin/bash\necho ECS_CLUSTER=my-cluster >> /etc/ecs/ecs.config"
-    instance_type        = "t2.micro"
-}
+# # Autoscaling group
+# ### DECIDE WHAT TO DO WITH AMI
+# resource "aws_launch_configuration" "ecs_launch_config" {
+#     image_id             = "ami-094d4d00fd7462815"
+#     iam_instance_profile = aws_iam_instance_profile.ecs_agent.name
+#     security_groups      = [aws_security_group.ecs_sg.id]
+#     user_data            = "#!/bin/bash\necho ECS_CLUSTER=my-cluster >> /etc/ecs/ecs.config"
+#     instance_type        = "t2.micro"
+# }
 
-resource "aws_autoscaling_group" "failure_analysis_ecs_asg" {
-    name                      = "asg"
-    vpc_zone_identifier       = [aws_subnet.pub_subnet.id]
-    launch_configuration      = aws_launch_configuration.ecs_launch_config.name
+# resource "aws_autoscaling_group" "failure_analysis_ecs_asg" {
+#     name                      = "asg"
+#     vpc_zone_identifier       = [aws_subnet.pub_subnet.id]
+#     launch_configuration      = aws_launch_configuration.ecs_launch_config.name
 
-    desired_capacity          = 2
-    min_size                  = 1
-    max_size                  = 10
-    health_check_grace_period = 300
-    health_check_type         = "EC2"
-}
+#     desired_capacity          = 2
+#     min_size                  = 1
+#     max_size                  = 10
+#     health_check_grace_period = 300
+#     health_check_type         = "EC2"
+# }
 
-# Database
-resource "aws_db_subnet_group" "db_subnet_group" {
-    subnet_ids  = [aws_subnet.pub_subnet.id]
-}
-### REMOVE PASSWORDS
-resource "aws_db_instance" "mysql" {
-    identifier                = "mysql"
-    allocated_storage         = 5
-    backup_retention_period   = 2
-    backup_window             = "01:00-01:30"
-    maintenance_window        = "sun:03:00-sun:03:30"
-    multi_az                  = true
-    engine                    = "mysql"
-    engine_version            = "5.7"
-    instance_class            = "db.t2.micro"
-    db_name                   = "worker_db"
-    username                  = "worker"
-    password                  = "worker"
-    port                      = "3306"
-    db_subnet_group_name      = aws_db_subnet_group.db_subnet_group.id
-    vpc_security_group_ids    = [aws_security_group.rds_sg.id, aws_security_group.ecs_sg.id]
-    skip_final_snapshot       = true
-    final_snapshot_identifier = "worker-final"
-    publicly_accessible       = true
-}
+# # Database
+# resource "aws_db_subnet_group" "db_subnet_group" {
+#     subnet_ids  = [aws_subnet.pub_subnet.id]
+# }
+# ### REMOVE PASSWORDS
+# resource "aws_db_instance" "mysql" {
+#     identifier                = "mysql"
+#     allocated_storage         = 5
+#     backup_retention_period   = 2
+#     backup_window             = "01:00-01:30"
+#     maintenance_window        = "sun:03:00-sun:03:30"
+#     multi_az                  = true
+#     engine                    = "mysql"
+#     engine_version            = "5.7"
+#     instance_class            = "db.t2.micro"
+#     db_name                   = "worker_db"
+#     username                  = "worker"
+#     password                  = "worker"
+#     port                      = "3306"
+#     db_subnet_group_name      = aws_db_subnet_group.db_subnet_group.id
+#     vpc_security_group_ids    = [aws_security_group.rds_sg.id, aws_security_group.ecs_sg.id]
+#     skip_final_snapshot       = true
+#     final_snapshot_identifier = "worker-final"
+#     publicly_accessible       = true
+# }
 
-# ECR
-resource "aws_ecr_repository" "worker" {
-    name  = "worker"
-}
+# # ECR
+# resource "aws_ecr_repository" "worker" {
+#     name  = "worker"
+# }
 
-# ECS
-resource "aws_ecs_cluster" "ecs_cluster" {
-    name  = "my-cluster"
-}
+# # ECS
+# resource "aws_ecs_cluster" "ecs_cluster" {
+#     name  = "my-cluster"
+# }
 
-data "template_file" "task_definition_template" {
-    template = file("task_definition.json.tpl")
-    vars = {
-      REPOSITORY_URL = replace(aws_ecr_repository.worker.repository_url, "https://", "")
-    }
-}
+# data "template_file" "task_definition_template" {
+#     template = file("task_definition.json.tpl")
+#     vars = {
+#       REPOSITORY_URL = replace(aws_ecr_repository.worker.repository_url, "https://", "")
+#     }
+# }
 
-resource "aws_ecs_task_definition" "task_definition" {
-  family                = "worker"
-  container_definitions = data.template_file.task_definition_template.rendered
-}
+# resource "aws_ecs_task_definition" "task_definition" {
+#   family                = "worker"
+#   container_definitions = data.template_file.task_definition_template.rendered
+# }
 
-resource "aws_ecs_service" "worker" {
-  name            = "worker"
-  cluster         = aws_ecs_cluster.ecs_cluster.id
-  task_definition = aws_ecs_task_definition.task_definition.arn
-  desired_count   = 2
-}
+# resource "aws_ecs_service" "worker" {
+#   name            = "worker"
+#   cluster         = aws_ecs_cluster.ecs_cluster.id
+#   task_definition = aws_ecs_task_definition.task_definition.arn
+#   desired_count   = 2
+# }
