76b1a769d4aea725396f25f9ffb60b4f1a89042f
Rafa Adorna <rafael.adorna@gmail.com>
2022-09-27 09:29:42 +0200

    Change defaults, add CIDR variable

--- infra-as-code/environments/test/variables.tf
+++ infra-as-code/environments/test/variables.tf
@@ -1,7 +1,7 @@
 variable "region" {
   type        = string
   description = "Currently mono region. Region where to deploy."
-  default     = "eu-west-2"
+  default     = "eu-west-1"
 }
 
 variable "name" {
@@ -13,5 +13,9 @@ variable "name" {
 variable "read_only_users" {
   type        = list
   description = "List of users to create with read only access."
-  default     = ["test-01","test-02"]
+}
+
+variable "main_cidr_block" {
+  type        = string
+  description = "CIDR block of IPs for the VPC."
 }
