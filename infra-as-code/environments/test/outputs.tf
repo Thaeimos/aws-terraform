3af58f343b09b472cb4c10e55319e66d6c7f6162
Rafa Adorna <rafael.adorna@gmail.com>
2022-09-27 16:01:23 +0200

    Disable passwords as output. Add DNS name

--- infra-as-code/environments/test/outputs.tf
+++ infra-as-code/environments/test/outputs.tf
@@ -1,4 +1,8 @@
-output "password" {
-  value     = values(aws_iam_user_login_profile.user_login).*.password
-  sensitive = true
+# output "password" {
+#   value     = values(aws_iam_user_login_profile.user_login).*.password
+#   sensitive = true
+# }
+
+output "dns_entry_url" {
+  value     = "http://${aws_lb.front_end.dns_name}"
 }
