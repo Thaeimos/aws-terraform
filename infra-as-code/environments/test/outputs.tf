2807d653118c66f1b60cc610671444c3fd3747f4
Rafa Adorna <rafael.adorna@gmail.com>
2022-09-28 16:09:39 +0200

    Add outputs needed by the frontend app

--- infra-as-code/environments/test/outputs.tf
+++ infra-as-code/environments/test/outputs.tf
@@ -6,3 +6,15 @@
 output "dns_entry_url" {
   value     = "http://${aws_lb.front_end.dns_name}"
 }
+
+output "ECR_REPOSITORY_FRONT" {
+  value     = aws_ecr_repository.docker_repo_frontend.name
+}
+
+output "ECS_CLUSTER_FRONT" {
+  value     = aws_ecs_cluster.ecs_cluster_frontend.name
+}
+
+# output "ECS_SERVICE_FRONT" {
+#   value     = ""
+# }
