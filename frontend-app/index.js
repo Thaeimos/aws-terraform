99c83a64ae391b6d8fe04f298bab71ec459725a5
Rafa Adorna <rafael.adorna@gmail.com>
2022-09-29 15:32:59 +0200

    Retrieve zone from EC2 API

--- frontend-app/index.js
+++ frontend-app/index.js
@@ -1,8 +1,19 @@
 const express = require('express')
+var child_process = require('child_process');
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+function runCmd(cmd)
+{
+  var resp = child_process.execSync(cmd);
+  var result = resp.toString('UTF8');
+  return result;
+}
+
+var cmd = "curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone";  
+var zone = runCmd(cmd);
+
+app.get('/', (req, res) => res.send(`Hello World from ${zone}!`))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
