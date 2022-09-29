07f3d62d9c83586c7703614710e33bebed7b8618
Rafa Adorna <rafael.adorna@gmail.com>
2022-09-29 18:32:38 +0200

    Add registry for backend. Describe better the names.

--- infra-as-code/environments/test/outputs.tf
+++ infra-as-code/environments/test/outputs.tf
@@ -7,6 +7,10 @@ output "MAIN_ENTRY_WEB" {
   value     = "http://${aws_lb.front_end.dns_name}"
 }
 
-output "FRONT_PLACEHOLDER_URL" {
+output "FRONT_PLACEHOLDER_URL_REGISTRY" {
   value     = aws_ecr_repository.docker_repo_frontend.repository_url
+}
+
+output "BACK_PLACEHOLDER_URL_REGISTRY" {
+  value     = aws_ecr_repository.docker_repo_backend.repository_url
 }
