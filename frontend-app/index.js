6536ddd4779be24cfc7f086b7b8ada8ffc03db20
Rafa Adorna <rafael.adorna@gmail.com>
2022-10-01 10:54:59 +0200

    Use newline

--- frontend-app/index.js
+++ frontend-app/index.js
@@ -10,9 +10,9 @@ app.get('/', async (req, res) => {
   fetch('http://169.254.169.254/latest/meta-data/hostname').then(async(response) => {
     const hostname = await response.text();
     console.log("Received a / request!");
-    res.write(`Hello from ${hostname} <br/>`)
-    res.write(`The loadbalancer for the backend is ${process.env.APPLICATION_LOAD_BALANCER}<br/>`)
-    res.write(`The environment value is ${process.env.ENVIRONMENT}<br/>`)
+    res.write(`Hello from ${hostname}\n`)
+    res.write(`The loadbalancer for the backend is ${process.env.APPLICATION_LOAD_BALANCER}\n`)
+    res.write(`The environment value is ${process.env.ENVIRONMENT}\n`)
     res.end()
     res.send()
   })
