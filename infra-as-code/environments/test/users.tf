961972a95a01f770a70dae2b663e8d513fe2cb2c
Rafa Adorna <rafael.adorna@gmail.com>
2022-10-03 17:07:55 +0200

    Prevent destruction of users

--- infra-as-code/environments/test/users.tf
+++ infra-as-code/environments/test/users.tf
@@ -2,6 +2,10 @@
 resource "aws_iam_group" "read_only" {
   name = "read-only-users"
   path = "/"
+
+  lifecycle {
+        prevent_destroy = true
+    } 
 }
 
 data "aws_iam_policy" "read_only_access" {
@@ -11,18 +15,30 @@ data "aws_iam_policy" "read_only_access" {
 resource "aws_iam_group_policy_attachment" "read_only" {
   group      = aws_iam_group.read_only.name
   policy_arn = data.aws_iam_policy.read_only_access.arn
+
+  lifecycle {
+        prevent_destroy = true
+    } 
 }
 
 resource "aws_iam_user" "user" {
   for_each      = toset(var.read_only_users)
   name          = each.value
-  force_destroy = true
+  force_destroy = false
+
+  lifecycle {
+        prevent_destroy = true
+    } 
 }
 
 resource "aws_iam_user_group_membership" "devstream" {
   for_each    = toset(var.read_only_users)
   user        = "${aws_iam_user.user[each.key].name}"
   groups      = [aws_iam_group.read_only.name]
+
+  lifecycle {
+        prevent_destroy = true
+    } 
 }
 
 resource "aws_iam_user_login_profile" "user_login" {
@@ -41,4 +57,8 @@ require_symbols                = false
 allow_users_to_change_password = true
 max_password_age               = 90  
 password_reuse_prevention      = 3
+
+lifecycle {
+        prevent_destroy = true
+    } 
 }
