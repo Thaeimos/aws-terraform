f11c25af71f9812f44d01dfeee524d2e32ccc9d5
Rafa Adorna <rafael.adorna@gmail.com>
2022-10-03 12:32:09 +0200

    Uncomment and prepare init users

--- backend-app/index.js
+++ backend-app/index.js
@@ -18,7 +18,6 @@ try {
 }
 
 let app = express()
-const APPLICATION_LOAD_BALANCER = process.env.APPLICATION_LOAD_BALANCER;
 
 app.get('/', async (req, res) => {
   res.send({ message: "Hello world from the backend" })
@@ -26,17 +25,22 @@ app.get('/', async (req, res) => {
 
 app.get('/init', async (req, res) => {
   console.log("Received a /init request!");
-  // connection.query('CREATE TABLE IF NOT EXISTS users (id INT(5) NOT NULL AUTO_INCREMENT PRIMARY KEY, lastname VARCHAR(40), firstname VARCHAR(40), email VARCHAR(30));');
-  // connection.query('INSERT INTO users (lastname, firstname, email) VALUES ( "Tony", "Sam", "tonysam@whatever.com"), ( "Doe", "John", "john.doe@whatever.com" );');
-  res.send({ message: "init step done" })
+  try {
+    connection.query('CREATE TABLE IF NOT EXISTS users (id INT(5) NOT NULL AUTO_INCREMENT PRIMARY KEY, lastname VARCHAR(40), firstname VARCHAR(40), email VARCHAR(30));');
+    connection.query('INSERT INTO users (lastname, firstname, email) VALUES ( "Tony", "Sam", "tonysam@whatever.com"), ( "Doe", "John", "john.doe@whatever.com" );');
+    res.send({ message: "init step done" })
+  } catch (error) {
+    console.log(`Error doing the init DB: ${error}`);
+    res.send({ message: "init step not OK :(" })
+  }
 })
 
 app.get('/users', async (req, res) => {
   console.log("Received a /users request!");
-  // connection.query('SELECT * from users', function (error, results) {
-  //   if (error) throw error;
-  //   res.send(results)
-  // });
+  connection.query('SELECT * from users', function (error, results) {
+    if (error) throw error;
+    res.send(results)
+  });
 })
 
 app.get('/test-back', async (req, res) => {
