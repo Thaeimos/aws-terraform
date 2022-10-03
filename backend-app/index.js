0e6c28fa1597d0bea4fc7f61f636356a05d483bb
Rafa Adorna <rafael.adorna@gmail.com>
2022-10-03 11:35:17 +0200

    Restore DB connectivity and operations

--- backend-app/index.js
+++ backend-app/index.js
@@ -1,17 +1,16 @@
 import express from 'express';
-import fetch from 'node-fetch';
 import mysql from 'mysql';
 
 const PORT = process.env.PORT || 3000
-// const connection = mysql.createConnection({
-//   host: process.env.RDS_HOSTNAME,
-//   user: process.env.RDS_USERNAME,
-//   password: process.env.RDS_PASSWORD,
-//   port: process.env.RDS_PORT,
-//   db_name: process.env.RDS_DB_NAME
-// });
-// connection.connect()
-// connection.query(`use ${process.env.RDS_DB_NAME};`)
+const connection = mysql.createConnection({
+  host: process.env.RDS_HOSTNAME,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  port: process.env.RDS_PORT,
+  db_name: process.env.RDS_DB_NAME
+});
+connection.connect()
+connection.query(`use ${process.env.RDS_DB_NAME};`)
 
 let app = express()
 const APPLICATION_LOAD_BALANCER = process.env.APPLICATION_LOAD_BALANCER;
@@ -22,17 +21,17 @@ app.get('/', async (req, res) => {
 
 app.get('/init', async (req, res) => {
   console.log("Received a /init request!");
-  // connection.query('CREATE TABLE IF NOT EXISTS users (id INT(5) NOT NULL AUTO_INCREMENT PRIMARY KEY, lastname VARCHAR(40), firstname VARCHAR(40), email VARCHAR(30));');
-  // connection.query('INSERT INTO users (lastname, firstname, email) VALUES ( "Tony", "Sam", "tonysam@whatever.com"), ( "Doe", "John", "john.doe@whatever.com" );');
+  connection.query('CREATE TABLE IF NOT EXISTS users (id INT(5) NOT NULL AUTO_INCREMENT PRIMARY KEY, lastname VARCHAR(40), firstname VARCHAR(40), email VARCHAR(30));');
+  connection.query('INSERT INTO users (lastname, firstname, email) VALUES ( "Tony", "Sam", "tonysam@whatever.com"), ( "Doe", "John", "john.doe@whatever.com" );');
   res.send({ message: "init step done" })
 })
 
 app.get('/users', async (req, res) => {
   console.log("Received a /users request!");
-  // connection.query('SELECT * from users', function (error, results) {
-  //   if (error) throw error;
-  //   res.send(results)
-  // });
+  connection.query('SELECT * from users', function (error, results) {
+    if (error) throw error;
+    res.send(results)
+  });
 })
 
 app.get('/test-back', async (req, res) => {
