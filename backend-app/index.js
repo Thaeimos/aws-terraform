ebeb404f4903e4c1b09b1a0212493cdb02c1ef98
Rafa Adorna <rafael.adorna@gmail.com>
2022-10-03 17:12:15 +0200

    Separate connections. Enable user query

--- backend-app/index.js
+++ backend-app/index.js
@@ -3,25 +3,6 @@ import mysql from 'mysql';
 
 const PORT = process.env.PORT || 3000
 
-try {
-  let connection = mysql.createConnection({
-    host: process.env.RDS_HOSTNAME,
-    user: process.env.RDS_USERNAME,
-    password: process.env.RDS_PASSWORD,
-    port: process.env.RDS_PORT,
-    database: process.env.RDS_DB_NAME
-  });
-  console.log("POST create connection");
-
-  connection.connect(error => {
-    if (error) throw error;
-    console.log("Successfully connected to the database.");
-  });
-
-} catch (error) {
-  console.log(`Error creating the connection to the DB: ${error}`);
-}
-
 let app = express()
 
 app.get('/', async (req, res) => {
@@ -32,6 +13,19 @@ app.get('/init', async (req, res) => {
   console.log("Received a /init request!");
 
   try {
+    let connection = mysql.createConnection({
+      host: process.env.RDS_HOSTNAME,
+      user: process.env.RDS_USERNAME,
+      password: process.env.RDS_PASSWORD,
+      port: process.env.RDS_PORT,
+      database: process.env.RDS_DB_NAME
+    });
+    console.log("POST create connection");
+  
+    connection.connect(error => {
+      if (error) throw error;
+      console.log("Successfully connected to the database.");
+    });
 
     console.log("PRE create table");
     let createTodos = `CREATE TABLE IF NOT EXISTS users (
@@ -65,12 +59,8 @@ app.get('/init', async (req, res) => {
         return console.log(err.message);
       }
     });
-
-    // connection.query('CREATE TABLE IF NOT EXISTS users (id INT(5) NOT NULL AUTO_INCREMENT PRIMARY KEY, lastname VARCHAR(40), firstname VARCHAR(40), email VARCHAR(30));');
-    // console.log("POST query 01");
-    // connection.query('INSERT INTO users (lastname, firstname, email) VALUES ( "Tony", "Sam", "tonysam@whatever.com"), ( "Doe", "John", "john.doe@whatever.com" );');
-    // console.log("POST query 02");
     res.send({ message: "init step done" })
+
   } catch (error) {
       console.log(`Error doing the init DB: ${error}`);
       res.send({ message: "init step not OK :(" })
@@ -80,10 +70,29 @@ app.get('/init', async (req, res) => {
 app.get('/users', async (req, res) => {
   console.log("Received a /users request!");
 
-  connection.query('SELECT * from users', function (error, results) {
-    if (error) throw error;
-    res.send({ message: `${results}` })
-  });
+  try {
+    let connection = mysql.createConnection({
+      host: process.env.RDS_HOSTNAME,
+      user: process.env.RDS_USERNAME,
+      password: process.env.RDS_PASSWORD,
+      port: process.env.RDS_PORT,
+      database: process.env.RDS_DB_NAME
+    });
+    console.log("POST create connection");
+  
+    connection.connect(error => {
+      if (error) throw error;
+      console.log("Successfully connected to the database.");
+    });
+
+    connection.query('SELECT * from users', function (error, results) {
+      if (error) throw error;
+      res.send({ message: `${results}` })
+    });
+  } catch (error) {
+    console.log(`Error doing the querying for users: ${error}`);
+    res.send({ message: "User query not OK :(" })
+}
 })
 
 app.get('/test-back', async (req, res) => {
