c225737b6c9e5245ebc6a7c00afaf28220e86c28
Rafa Adorna <rafael.adorna@gmail.com>
2022-10-14 00:27:20 +0200

    Use disctinct name for ECR Xray

--- infra-as-code/environments/production/outputs.tf
+++ infra-as-code/environments/production/outputs.tf
@@ -34,7 +34,7 @@ output "BACK_DB_SECRET_GROUP" {
   value = aws_secretsmanager_secret.secretmasterDB.arn
 }
 
-output "BACK_REG_URL" {
+output "XRAY_REG_URL" {
   value = aws_ecr_repository.docker_repo_backend_xray.repository_url
   description = "The URL for the XRAY image for private subnets."
 }
